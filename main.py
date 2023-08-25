9c34eb8c8920fda74087dd7b15c03d80b919a236
jialianxing <jialianxing@piesat.cn>
2023-08-25 17:08:20 +0800

    开始

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
+# coding:utf-8
+import os
 import sys
-from PyQt5.QtWidgets import QApplication
-from mainForm import mainFrom
 
+from PyQt5.QtCore import Qt, QTranslator, QUrl
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication
+from qfluentwidgets import FluentTranslator
+
+from app.common.config import cfg
+from app.view.main_window import MainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
+
+# enable dpi scale
+if cfg.get(cfg.dpiScale) == "Auto":
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+else:
+    os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = str(cfg.get(cfg.dpiScale))
+
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+# create application
 app = QApplication(sys.argv)
-win = mainFrom()
-win.show()
-sys.exit(app.exec_())
+app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
+
+# internationalization
+locale = cfg.get(cfg.language).value
+translator = FluentTranslator(locale)
+galleryTranslator = QTranslator()
+galleryTranslator.load(locale, "gallery", ".", ":/gallery/i18n")
+
+app.installTranslator(translator)
+app.installTranslator(galleryTranslator)
+
+# create main window
+w = MainWindow()
+
+
+
+w.show()
+
+app.exec_()
--- test.py
+++
@@ -1,6 +0,0 @@
-import sys
-import numpy
-import os
-
-if __name__ == '__main__':
-    print('1')
